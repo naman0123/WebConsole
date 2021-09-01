c64b071b2804ea94a41d098b0c3f688a3a7bfca6
naman0123 <83550704+naman0123@users.noreply.github.com>
2021-09-01 19:35:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var first_animal = "dogs";
+var second_animal = "cats";
+var both_animals = first_animal.concat(second_animal);
+
+console.log(both_animals);
+
+var a = 4;
+var b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
